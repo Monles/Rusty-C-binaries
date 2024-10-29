628e9b94f589401187c63f88fd1d8290757e28a0
Yen Wang <73730090+Monles@users.noreply.github.com>
2024-10-29 17:55:47 +0000

    up

---
+++ Rust/03-entry-profiles/basic/target/debug/basic.d
@@ -0,0 +1,1 @@
+/Users/lesmo/Documents/Github/Rusty-C-binaries/Rust/03-entry-profiles/basic/target/debug/basic: /Users/lesmo/Documents/Github/Rusty-C-binaries/Rust/03-entry-profiles/basic/src/main.rs
---
+++ Rust/03-entry-profiles/basic/target/debug/deps/basic-f15d24b198c72092.d
@@ -0,0 +1,5 @@
+/Users/lesmo/Documents/Github/Rusty-C-binaries/Rust/03-entry-profiles/basic/target/debug/deps/basic-f15d24b198c72092: src/main.rs
+
+/Users/lesmo/Documents/Github/Rusty-C-binaries/Rust/03-entry-profiles/basic/target/debug/deps/basic-f15d24b198c72092.d: src/main.rs
+
+src/main.rs:
